807a75eeaa25e32307b908a24d33e90780c6facd
Starlight Romero <starlightromero@gmail.com>
2020-10-13 16:39:24 -0700

    created User model

--- hamming_messages/models.py
+++ hamming_messages/models.py
@@ -1,8 +1,29 @@
 from datetime import datetime
-from hamming_messages import db
+from flask_login import UserMixin
+from hamming_messages import db, login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Get current logged in user."""
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    """User database class."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image = db.Column(db.String(20), nullable=False, default="default.jpg")
+    password = db.Column(db.String(60), nullable=True)
+    messages = db.relationship("Message", backref="sender", lazy=True)
 
 
 class Message(db.Model):
+    """Message database class."""
+
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(500), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
+    sender_id = db.Column(db.Integer, db.ForeignKey("user.id"))
