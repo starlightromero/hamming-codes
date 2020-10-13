3faf0a9bb900446e6b3bc8dffc45eb585274fe10
Starlight Romero <starlightromero@gmail.com>
2020-10-13 16:48:56 -0700

    set up user forms

---
+++ hamming_messages/users/forms.py
@@ -0,0 +1,55 @@
+from flask_login import current_user
+from flask_wtf import FlaskForm
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+from wtforms.fields import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+)
+from wtforms.fields.html5 import EmailField
+from hamming_messages.models import User
+
+
+class SignupForm(FlaskForm):
+    """User signup form."""
+
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=8, max=20)]
+    )
+    submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        """Validate username is not taken."""
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "That username is taken. Please choose a different username."
+            )
+
+    def validate_email(self, email):
+        """Validate email is not in use."""
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                """
+                The email is already in use. Please enter a different email.
+                Alternatively, if you forgot your password, go to the signin
+                page and click \"Forgot Password\".
+                """
+            )
+
+
+class SigninForm(FlaskForm):
+    """User signin form."""
+
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField(
+        "Password", validators=[DataRequired(), Length(min=8, max=20)]
+    )
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
