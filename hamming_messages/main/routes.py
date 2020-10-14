b14864a18e31a430bf00200bdf93f252bb771f3b
Starlight Romero <starlightromero@gmail.com>
2020-10-14 08:49:43 -0700

    added login_required to home

--- hamming_messages/main/routes.py
+++ hamming_messages/main/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, Blueprint
+from flask_login import login_required
 from flask_socketio import send
 from hamming_messages import socketio, db
 from hamming_messages.models import Message
@@ -16,6 +17,7 @@ def handle_message(msg):
 
 
 @main.route("/")
+@login_required
 def home():
     """Render home page."""
     messages = Message.query.all()
