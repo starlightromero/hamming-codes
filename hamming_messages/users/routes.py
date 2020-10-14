cc1a3c50d3b4b0ecb646bf8a2672a5ef8fc1b7f7
Starlight Romero <starlightromero@gmail.com>
2020-10-13 17:03:03 -0700

    created signin and sign up routes

--- hamming_messages/users/routes.py
+++ hamming_messages/users/routes.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, Blueprint
+from flask import (
+    Flask,
+    render_template,
+    Blueprint,
+    request,
+    redirect,
+    url_for,
+    flash,
+)
+from flask_login import current_user, login_required, login_user, logout_user
 from hamming_messages import db
 from hamming_messages.models import User
+from hamming_messages.users.forms import SigninForm, SignupForm
 
 users = Blueprint("users", __name__)
 
@@ -9,3 +19,58 @@ users = Blueprint("users", __name__)
 def welcome():
     """Render welcome page."""
     return render_template("welcome.pug")
+
+
+@users.route("/signin", methods=["GET", "POST"])
+def signin():
+    """Render signin page."""
+    if current_user.is_authenticated:
+        redirect(url_for("main.home"))
+    form = SigninForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return (
+                redirect(next_page)
+                if next_page
+                else redirect(url_for("main.home"))
+            )
+        flash("Sign in unsuccessful. Please verify email and password.")
+    context = {
+        "title": "Sign In",
+        "form": form,
+    }
+    return render_template("signin.pug", **context)
+
+
+@users.route("/signup", methods=["GET", "POST"])
+def signup():
+    """Render signup page."""
+    if current_user.is_authenticated:
+        redirect(url_for("main.home"))
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Sign up successful!")
+        return redirect(url_for("main.home"))
+    context = {
+        "title": "Sign Up",
+        "form": form,
+    }
+    return render_template("signup.pug", **context)
+
+
+@users.route("/signout")
+@login_required
+def logout():
+    """Sign out current user and redirect to welcome."""
+    logout_user()
+    return redirect(url_for("users.welcome"))
