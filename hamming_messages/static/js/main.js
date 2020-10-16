6368bf21b2a5a1074f35c947d44869754eb27d9c
Starlight Romero <starlightromero@gmail.com>
2020-10-16 12:15:58 -0700

    added scroll function. added message class differentiation

--- hamming_messages/static/js/main.js
+++ hamming_messages/static/js/main.js
@@ -4,6 +4,10 @@ const newMessage = document.getElementById('newMessage')
 const sender = document.getElementById('sender').innerHTML
 const onlineUsersList = document.getElementById('onlineUsers')
 
+const scrollBottom = () => {
+  messages.scrollTop = messages.scrollHeight
+}
+
 let socket = io.connect('http://127.0.0.1:5000')
 
 socket.on('connect', () => {
@@ -13,22 +17,27 @@ socket.on('connect', () => {
 socket.on('message', data => {
   if (data['message'] && data['sender']) {
     const ul = document.createElement('ul')
-    const sender = document.createElement('li')
-    const message = document.createElement('li')
-    sender.appendChild(document.createTextNode(data['sender']))
-    message.appendChild(document.createTextNode(data['message']))
-    ul.appendChild(sender)
-    ul.appendChild(message)
+    data['sender'] === sender ? ul.classList.add('sent-message') : ul.classList.add('received-message')
+    const senderli = document.createElement('li')
+    const messageli = document.createElement('li')
+    senderli.appendChild(document.createTextNode(data['sender']))
+    messageli.appendChild(document.createTextNode(data['message']))
+    senderli.classList.add('sender')
+    messageli.classList.add('message')
+    ul.appendChild(senderli)
+    ul.appendChild(messageli)
     messages.appendChild(ul)
   } else {
     const p = document.createElement('p')
     p.appendChild(document.createTextNode(data))
     messages.appendChild(p)
   }
+  scrollBottom()
 })
 
 socket.on('usernameConnected', data => {
   console.log(data)
+  scrollBottom()
   // const p = document.createElement('p')
   // p.appendChild(document.createTextNode(data))
   // messages.appendChild(p)
@@ -48,16 +57,16 @@ socket.on('disconnect', () => {
 
 socket.on('usernameDisconnected', data => {
   console.log(data)
-  const p = document.createElement('p')
-  p.appendChild(document.createTextNode(data))
-  messages.appendChild(p)
-  const onlineUsers = onlineUsersList.querySelectorAll('li')
-  for (user of onlineUsers) {
-    console.log(user.innerHTML)
-    if (user.innerHTML === username) {
-      console.log('yes')
-    }
-  }
+  // const p = document.createElement('p')
+  // p.appendChild(document.createTextNode(data))
+  // messages.appendChild(p)
+  // const onlineUsers = onlineUsersList.querySelectorAll('li')
+  // for (user of onlineUsers) {
+  //   console.log(user.innerHTML)
+  //   if (user.innerHTML === username) {
+  //     console.log('yes')
+  //   }
+  // }
 })
 
 newMessage.addEventListener('keyup', event => {
