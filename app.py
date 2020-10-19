f9824437be61d71b512d46ef14213c5b59d9c80d
Starlight Romero <starlightromero@gmail.com>
2020-10-19 13:14:35 -0700

    updated app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Import and run app."""
-from hamming_messages import create_app
+from hamming_messages import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    socketio.run(app)
